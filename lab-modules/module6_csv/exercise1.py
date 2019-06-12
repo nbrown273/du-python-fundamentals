54d2521a2a0a34145ac83f966ae19a8a24f6a024
Nathan Brown <nathan.brown@daugherty.com>
2019-06-12 16:47:00 -0500

    start at csv handling exercise

--- lab-modules/module6_csv/exercise1.py
+++ lab-modules/module6_csv/exercise1.py
@@ -1,2 +1,12 @@
 # STOP HERE: If you have finished the json exercises within the first day of modules, 
 # use this time to review the past content and aid your peers. 
+
+# You've probably already heard of CSV files, but are you aware that there is no agreed upon standard
+# for CSV file formats? As a result, it can be very helpful to have a csv module that encapsulates the
+# same behavior for all the common CSV formats. It also allows custom CSV formats to be defined, and defines
+# helpful utilities for converting between CSV and python dictionaries.
+
+# The following code writes a series of dictionaries to an excel compatible csv file.
+data = [
+    {'abc' }
+]
